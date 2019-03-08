610fcd1785d8fd57c9d1cbe85737e343dae66725
Shane D Wilson <me@shanedwilson.com>
2019-03-08 17:32:29 -0600

    Add project files.

---
+++ Simpler Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simpler_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
