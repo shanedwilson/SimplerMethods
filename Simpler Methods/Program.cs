67a843d52908ce42574cea15e4e5838563a02db1
Shane D Wilson <me@shanedwilson.com>
2019-03-08 18:07:36 -0600

    working

--- Simpler Methods/Program.cs
+++ Simpler Methods/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Simpler_Methods
 {
@@ -6,7 +7,16 @@ namespace Simpler_Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var prayingMantis = new Bug("Praying Mantis", "Religiosa",
+                new List<string> { "people" },
+                new List<string> { "moths", "mosquitoes", "roaches", "flies", "aphids" });
+
+            Console.WriteLine(prayingMantis.PreyList());
+            Console.WriteLine(prayingMantis.PredatorList());
+            Console.WriteLine(prayingMantis.Eat("people"));
+            Console.WriteLine(prayingMantis.Eat("flies"));
+            Console.ReadKey();
         }
     }
+
 }
